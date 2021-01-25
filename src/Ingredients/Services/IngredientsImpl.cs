003f371802290a3b45ee450531b909e4da096a48
Mark Rendle <mark@markrendle.net>
2021-01-25 10:10:37 +0000

    Add Ingredients

---
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Pizza.Data;
+
+namespace Ingredients.Services
+{
+    public class IngredientsImpl : IngredientsService.IngredientsServiceBase
+    {
+        private readonly IToppingData _data;
+
+        public IngredientsImpl(IToppingData data)
+        {
+            _data = data;
+        }
+
+        public override async Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+        {
+            var toppings = await _data.GetAsync(context.CancellationToken);
+            var availableToppings = toppings.Select(t =>
+                new AvailableTopping
+                {
+                    Quantity = t.StockCount,
+                    Topping = new Topping
+                    {
+                        Id = t.Id,
+                        Name = t.Name,
+                        Price = (double)t.Price
+                    }
+                });
+
+            var response = new GetToppingsResponse
+            {
+                Toppings = {availableToppings}
+            };
+
+            return response;
+        }
+    }
+}
